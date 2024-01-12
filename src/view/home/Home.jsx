4c1c0883485c12bcbf65aececa960c3ed962b515
ricardo <ohcanikissu@gmail.com>
2024-01-12 09:53:25 +0800

    refactor:websocket

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -5,44 +5,22 @@ import Header from "../components/Header.jsx";
 import friendsData  from "../../data/friends.json";
 import ChatInput from "../components/ChatInput.jsx";
 import ChatPreview from "../components/ChatPreview.jsx";
+import useWebSocket from "react-use-websocket";
 const Home = () => {
     const [friends, setFriends] = useState(friendsData)
-    const [messages, setMessages] = useState([])
-    const [socket, setSocket] = useState(null);
+    const [messageHistory, setMessageHistory] = useState([]);
+
+    const socketUrl = 'ws://localhost:9999/ws/conn?userId=1';
+    const { sendMessage,sendJsonMessage , lastMessage, readyState } = useWebSocket(socketUrl);
 
     useEffect(() => {
-        // 创建 WebSocket 连接
-        const newSocket = new WebSocket('ws://localhost:8080/ws/conn?userId=1'); // 替换为你的 WebSocket 服务器地址
-
-        // 设置消息接收处理程序
-        newSocket.onmessage = (event) => {
-            const receivedMessage = JSON.parse(event.data);
-            setMessages((prevMessages) => [...prevMessages, receivedMessage]);
-        };
-
-        newSocket.onerror = (error) => {
-            console.error('WebSocket Error:', error);
-        };
-
-        // 设置关闭处理程序
-        newSocket.onclose = () => {
-            console.log('WebSocket Connection Closed');
-        };
-
-        // 将新的 WebSocket 对象存储在 state 中
-        setSocket(newSocket);
-
-        // 在组件卸载时关闭 WebSocket 连接
-        return () => {
-            newSocket.close();
-        };
-    }, []); // 空数组表示只在组件加载时运行一次
-
-    const sendMessage = (content) => {
-        if (socket && content.trim() !== '') {
-            // 发送消息到服务器
-            socket.send(JSON.stringify({ type: 2,from: 1,to: 1, content }));
+        if (lastMessage !== null) {
+            setMessageHistory((prev) => prev.concat(lastMessage));
         }
+    }, [lastMessage, setMessageHistory]);
+
+    const handleClick = (content) => {
+        sendJsonMessage({ type: 2,from: 1,to: 1, content });
     };
 
     return <div className={'w-full h-full'}>
@@ -52,9 +30,9 @@ const Home = () => {
             </div>
             <div className="flex-1 p-4 relative bg-gradient-to-bl">
                 <Header/>
-                <ChatPreview messages={messages}/>
+                <ChatPreview messages={messageHistory}/>
                 <div className={'absolute bottom-0 left-0 right-0'}>
-                    <ChatInput onSendMessage={(content) => {sendMessage(content)}} />
+                    <ChatInput onSendMessage={(content) => {handleClick(content)}} />
                 </div>
             </div>
         </div>
