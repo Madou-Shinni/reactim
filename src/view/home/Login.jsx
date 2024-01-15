0c9c69c7e8dc1095d8a1e35e3fe249fb43bb5f2d
ricardo <ohcanikissu@gmail.com>
2024-01-15 13:12:39 +0800

    refactor:登录

--- src/view/home/Login.jsx
+++ src/view/home/Login.jsx
@@ -3,8 +3,6 @@ import {UserUrl} from "@/api/user.js";
 import ErrorNotification from "@/view/components/error/ErrorNotification.jsx";
 import Loading from "@/view/components/loading/Loading.jsx";
 import useRequest from "@/hooks/useRequest.js";
-import {useEffect, useState} from "react";
-import AxiosInstance from "@/hooks/axiosInstance.js";
 
 const GithubLoginQuery = {
     clientId: import.meta.env.VITE_Github_ClientID,
@@ -15,48 +13,36 @@ const GithubLoginQuery = {
 const Login = () => {
     const navigate = useNavigate();
     const [query] = useSearchParams();
-    const [error, setError] = useState()
-    const [user, setUser] = useState()
     const code = query.get('code');
-    const handleLogin = async (code) => {
-        const {data,err} = await AxiosInstance({url: UserUrl.Login,params: {code: code}});
-        if (err) {
-            setError(err)
-        }
-        if (data) {
-            setUser(data.user)
-        }
-    }
-
-    useEffect(() => {
-        if (user) {
-            navigate(`/home/${user.id}`);
-        }
-        if (code) {
-            handleLogin(code);
-        }
-    }, [user, navigate]);
-
+    const {data,error} = useRequest({url: UserUrl.Login, params: {code: code}})
     if (error) {
-        return <ErrorNotification errorMessage={error} />;
+        return <ErrorNotification errorMessage={error.message} onClose={()=>{navigate(`/login/github`);}} />;
     }
-    if (code && !user) {
-        return <Loading />;
+    if (code && !data) {
+        return <Loading/>;
     }
-
-
-
+    if (data) {
+        navigate(`/home/${data.user.id}`);
+        return
+    }
 
 
     const handleAccessLogin = () => {
         // 重定向到 GitHub OAuth 授权页面
-        const url= `https://github.com/login/oauth/authorize?client_id=${GithubLoginQuery.clientId}&redirect_uri=${GithubLoginQuery.redirect_uri}&scope=${GithubLoginQuery.scope}`
+        const url = `https://github.com/login/oauth/authorize?client_id=${GithubLoginQuery.clientId}&redirect_uri=${GithubLoginQuery.redirect_uri}&scope=${GithubLoginQuery.scope}`
         window.location.href = url;
     };
 
-    return <div>
-        <h2>Login with GitHub</h2>
-        <button onClick={handleAccessLogin}>授权登录</button>
+    return <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
+            <h2 className="text-2xl font-semibold text-center mb-6">Login with GitHub</h2>
+            <button
+                className="w-full px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+                onClick={handleAccessLogin}
+            >
+                Login with GitHub
+            </button>
+        </div>
     </div>
 }
 
